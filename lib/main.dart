32267f4231ff25de36bf9e437cafceeef2056608
CarlosEliezer <carloseliezer73@gmail.com>
2021-05-26 21:08:23 -0300

    Dashboar Layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,66 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.deepOrange[500],
-            elevation: 12,
-            textTheme: TextTheme(headline6: TextStyle(fontSize: 23)),
-            title: Text('Dashboard'),
-            centerTitle: true,
+        theme: ThemeData(
+          primaryColor: Colors.grey[700],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
           ),
-        )
+        ),
+        home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 12,
+        textTheme: TextTheme(headline6: TextStyle(fontSize: 23)),
+        title: Text('Dashboard'),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: <Widget> [
+          Image.asset('images/bytebank_logo.png'),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
