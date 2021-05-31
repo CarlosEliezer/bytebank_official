a7a06aa4a1e57e7fa9e136ff2c04e50325cb60c3
CarlosEliezer <carloseliezer73@gmail.com>
2021-05-31 14:12:44 -0300

    Feature: Contacts_List

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'screens/contacts_list.dart';
+import 'screens/dashboard.dart';
+
 void main() {
   runApp(ByteBankApp());
 }
@@ -9,65 +12,14 @@ class ByteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          primaryColor: Colors.grey[700],
+          primaryColor: Colors.grey[800],
           accentColor: Colors.blueAccent[700],
           buttonTheme: ButtonThemeData(
             buttonColor: Colors.blueAccent[700],
             textTheme: ButtonTextTheme.primary,
           ),
         ),
-        home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 12,
-        textTheme: TextTheme(headline6: TextStyle(fontSize: 23)),
-        title: Text('Dashboard'),
-        centerTitle: true,
-      ),
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: <Widget> [
-          Image.asset('images/bytebank_logo.png'),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+        home: ContactsList(),
     );
   }
 }
